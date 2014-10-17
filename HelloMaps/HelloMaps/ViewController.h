15690d346a68c9eb56fee468cc58acc0cfbdfda5
Alexandr Khrulev <a_khrulev@MacBook-Pro-Alexandr.local>
2014-10-18 00:11:53 +0400

    created a project

---
+++ HelloMaps/HelloMaps/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloMaps
+//
+//  Created by Alexandr Khrulev on 18.10.14.
+//  Copyright (c) 2014 hackday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloMaps/HelloMaps/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloMaps
+//
+//  Created by Alexandr Khrulev on 18.10.14.
+//  Copyright (c) 2014 hackday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
