a865176c899a2ce26508435a4a0230ecb7eaff54
Makarychev Andrey <makarychev@reg.ru>
2014-10-18 11:49:41 +0400

    верстка login (анимация)

--- HelloMaps/HelloMaps/HMAuthorizationController.m
+++ HelloMaps/HelloMaps/HMAuthorizationController.m
@@ -9,6 +9,8 @@
 #import "HMAuthorizationController.h"
 
 @interface HMAuthorizationController ()
+@property (weak, nonatomic) IBOutlet UIView *hiddenView;
+@property (weak, nonatomic) IBOutlet UIImageView *logoView;
 
 @end
 
@@ -16,12 +18,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     [VKSdk initializeWithDelegate:self andAppId:VK_APP_ID];
     if ([VKSdk wakeUpSession])
     {
         [VKSdk authorize:@[VK_PER_FRIENDS, VK_PER_MESSAGES]];
     }
+    self.hiddenView.alpha = 0.0;
+}
+
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    __weak typeof(self) weakSelf = self;
+    [UIView animateWithDuration:0.6 animations:^{
+        CGRect frame = weakSelf.logoView.frame;
+        frame.origin.y = 71.00;
+        weakSelf.logoView.frame = frame;
+    }];
+    [UIView animateWithDuration:0.6 delay:0.25 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        weakSelf.hiddenView.alpha = 1.0;
+    } completion:nil
+     ];
+    
 }
 
 - (void)didReceiveMemoryWarning {
